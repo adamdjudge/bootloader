17b49d011a2f000fb5a0df28cf103bf6a1fd8e43
Adam Judge <adamdavidjudge@gmail.com>
2026-03-04 23:21:24 -0500

    Serial implementation updates and documentation

--- src/main.rs
+++ src/main.rs
@@ -11,21 +11,20 @@ use core::fmt::Write;
 use core::panic::PanicInfo;
 
 use console::{Color, Writer};
-use serial::{BaudRate, ComPort, SerialPort};
+use serial::{ComPort, SerialPort};
 
 global_asm!(include_str!("start.s"), options(att_syntax));
 
 #[unsafe(no_mangle)]
 fn main() -> ! {
-    let mut writer = Writer::get();
+    let writer = Writer::get();
     writer.clear_screen();
-    let _ = write!(&mut writer, "Loading kernel over COM1 at 19200 baud...\n");
+    let _ = write!(writer, "Loading kernel over COM1 at 19200 baud...\n");
 
-    let serial = SerialPort::get(ComPort::Com1);
-    serial.set_baud_rate(BaudRate::try_from(19200).unwrap());
-    serial.reset();
-
+    let serial = SerialPort::get(ComPort::Com1, 19200);
     let start_addr = serial::load_kernel(&serial);
+
+    // Jump to the start address of the kernel, ending bootloader execution.
     unsafe {
         asm!(
             "jmp eax",
@@ -35,23 +34,28 @@ fn main() -> ! {
     }
 }
 
+/// Global panic handler for the bootloader.
 #[inline(never)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    let mut writer = Writer::get();
+    unsafe {
+        asm!("cli");
+    }
+
+    let writer = Writer::get();
     writer.set_bg_color(Color::Black);
     writer.set_text_color(Color::LightRed);
 
     if let Some(location) = info.location() {
         let _ = write!(
-            &mut writer,
+            writer,
             "\npanicked at {}:{} - {}",
             location.file(),
             location.line(),
             info.message()
         );
     } else {
-        let _ = write!(&mut writer, "\npanicked - {}", info.message());
+        let _ = write!(writer, "\npanicked - {}", info.message());
     }
 
     loop {
--- src/serial.rs
+++ src/serial.rs
@@ -28,6 +28,7 @@ const MAX_BAUD: usize = 115200;
 /// Maximum number of program segments that can be loaded over serial
 const MAX_SEGMENTS: usize = 16;
 
+/// Serial port designation, either COM1 or COM2.
 #[allow(dead_code)]
 #[derive(Debug)]
 pub enum ComPort {
@@ -36,6 +37,7 @@ pub enum ComPort {
 }
 
 impl ComPort {
+    /// Returns I/O base address of the COM port.
     fn base_addr(&self) -> u16 {
         match self {
             ComPort::Com1 => 0x3f8,
@@ -44,32 +46,24 @@ impl ComPort {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
-pub struct BaudRate(usize);
-
-impl TryFrom<usize> for BaudRate {
-    type Error = usize;
-
-    fn try_from(value: usize) -> Result<Self, Self::Error> {
-        if value >= MIN_BAUD && value <= MAX_BAUD && MAX_BAUD % value == 0 {
-            Ok(BaudRate(value))
-        } else {
-            Err(value)
-        }
-    }
-}
-
+/// Handle for a serial port.
 pub struct SerialPort {
     base: u16,
 }
 
 impl SerialPort {
-    pub fn get(com: ComPort) -> Self {
-        Self {
+    /// Returns a handle to the specified serial port, configured to a given baud rate. Panics if
+    /// the baud rate is invalid.
+    pub fn get(com: ComPort, baudrate: usize) -> Self {
+        let serial = Self {
             base: com.base_addr(),
-        }
+        };
+        serial.set_baud_rate(baudrate);
+        serial.reset();
+        serial
     }
 
+    /// Resets the serial port.
     pub fn reset(&self) {
         unsafe {
             port::out8(self.base + IEN, 0x00); // Disable interrupts
@@ -78,8 +72,14 @@ impl SerialPort {
         }
     }
 
-    pub fn set_baud_rate(&self, baud: BaudRate) {
-        let divisor = MAX_BAUD / baud.0;
+    /// Configures the baud rate of the serial port. Panics if the given baud rate is invalid.
+    pub fn set_baud_rate(&self, rate: usize) {
+        assert!(
+            rate >= MIN_BAUD && rate <= MAX_BAUD && MAX_BAUD % rate == 0,
+            "tried to set invalid baud rate {rate}"
+        );
+
+        let divisor = MAX_BAUD / rate;
         unsafe {
             let lcr = port::in8(self.base + LCR);
             port::out8(self.base + LCR, lcr | 0x80); // Set DLAB
@@ -89,18 +89,19 @@ impl SerialPort {
         }
     }
 
-    pub fn receive_byte(&self) -> u8 {
+    /// Receives one byte from the serial port. Blocks execution until there is a byte available.
+    pub fn receive_u8(&self) -> u8 {
         unsafe {
             while port::in8(self.base + LSR) & 0x01 == 0 {}
             port::in8(self.base + FIFO)
         }
     }
 
+    /// Receives one 32-bit value from the serial port, transmitted in little endian order. Blocks
+    /// execution until 4 bytes have been received.
     pub fn receive_u32(&self) -> u32 {
         let mut bytes = [0u8; 4];
-        for i in 0..4 {
-            bytes[i] = self.receive_byte();
-        }
+        bytes.fill_with(|| self.receive_u8());
         u32::from_le_bytes(bytes)
     }
 }
@@ -111,15 +112,32 @@ struct Segment {
     size: u32,
 }
 
+/// Loads a kernel into memory over the given serial port, and returns its start address. Blocks
+/// execution until a full executable has been loaded. Panics if the received executable is invalid
+/// or if there is a checksum failure.
+/// 
+/// The kernel executable image received over serial is expected to contain a header, made of little
+/// endian u32 fields, followed by a byte stream of all the segments concatenated together. The
+/// header has the following format:
+///   - Start address (u32)
+///   - Number of segments (u32)
+///   - Array with entry for each segment:
+///     - Base address (u32)
+///     - Size in bytes (u32)
+///   - CRC-32 data checksum (u32)
+///   - CRC-32 header checksum (u32)
+/// 
+/// NOTE: Use sendelf.py to parse ELF executables and emit an image to send over serial.
 pub fn load_kernel(serial: &SerialPort) -> u32 {
-    let mut segments = [Segment::default(); MAX_SEGMENTS];
-    let mut writer = Writer::get();
+    let writer = Writer::get();
     let mut crc = Crc32::new();
 
+    // Receive start address dword.
     let start_addr = serial.receive_u32();
     crc.crc32_u32(start_addr);
-    let _ = write!(&mut writer, "start address: 0x{:08x}\n", start_addr);
+    let _ = write!(writer, "start address: 0x{:08x}\n", start_addr);
 
+    // Receive segments count dword.
     let segments_count = serial.receive_u32() as usize;
     crc.crc32_u32(segments_count as u32);
     assert!(
@@ -128,8 +146,10 @@ pub fn load_kernel(serial: &SerialPort) -> u32 {
         segments_count,
         MAX_SEGMENTS,
     );
-    let _ = write!(&mut writer, "segments count: {}\n", segments_count);
+    let _ = write!(writer, "segments count: {}\n", segments_count);
 
+    // Receive segments array. Each segment contains an address dword and size dword.
+    let mut segments = [Segment::default(); MAX_SEGMENTS];
     for i in 0..segments_count {
         let segment = Segment {
             addr: serial.receive_u32(),
@@ -140,42 +160,44 @@ pub fn load_kernel(serial: &SerialPort) -> u32 {
         segments[i] = segment;
     }
 
+    // Receive data checksum dword.
     let data_checksum = serial.receive_u32();
     crc.crc32_u32(data_checksum);
 
+    // Receive header checksum dword and check for correctness.
     let header_checksum = serial.receive_u32();
     let crc32 = crc.finish();
-    assert_eq!(
-        header_checksum, crc32,
+    assert!(
+        header_checksum == crc32,
         "header checksum 0x{:x} does not match computed CRC-32 0x{:x}",
-        header_checksum, crc32
+        header_checksum,
+        crc32
     );
 
+    // Receive data for each segment and load into memory, computing data checksum from all bytes.
+    let mut crc = Crc32::new();
     for segment in segments.iter().take(segments_count) {
         let _ = write!(
-            &mut writer,
+            writer,
             "load segment: addr=0x{:08x} size=0x{:08x}\n",
             segment.addr, segment.size
         );
-        let segment =
+
+        let segment_slice =
             unsafe { slice::from_raw_parts_mut(segment.addr as *mut u8, segment.size as usize) };
-        for i in 0..segment.len() {
-            segment[i] = serial.receive_byte();
-        }
+        segment_slice.fill_with(|| serial.receive_u8());
+        crc.crc32_slice(segment_slice);
     }
 
-    let mut crc = Crc32::new();
-    for segment in segments.iter().take(segments_count) {
-        crc.crc32_slice(unsafe {
-            slice::from_raw_parts_mut(segment.addr as *mut u8, segment.size as usize)
-        });
-    }
+    // Check data checksum for correctness.
     let crc32 = crc.finish();
-    assert_eq!(
-        data_checksum, crc32,
+    assert!(
+        data_checksum == crc32,
         "data checksum 0x{:x} does not match computed CRC-32 0x{:x}",
-        data_checksum, crc32
+        data_checksum,
+        crc32
     );
 
+    // Return the kernel executable start address so the bootloader can jump to it.
     start_addr
 }
